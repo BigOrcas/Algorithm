abb8149e5aff4fb480589b83ac01a0084be5d85a
bigwhale1948@gmail.com <bigwhale1948@gmail.com>
2022-12-19 13:36:01 +0900

    [221219 단어뒤집기 리팩토링]

--- Inflearn/_01_04.java
+++ Inflearn/_01_04.java
@@ -1,25 +1,26 @@
 import java.util.*;
 
-class Main{
-    public ArrayList<String> solution(String[] str,int n){
+
+class Main {
+    public ArrayList<String> solution(int n,String[] str){
         ArrayList<String> answer = new ArrayList<>();
         for(String x : str){
+            // StringBuilder객체로 변환 후 문자열을 뒤집고, 다시 스트링객체로 변환한다
             String tmp = new StringBuilder(x).reverse().toString();
             answer.add(tmp);
         }
         return answer;
     }
 
-    public static void main(String[] args){
-        Main m = new Main();
+    public static void main(String[] args) {
+        Main main = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String[] str = new String[n];
-
-        for(int i=0;i<n;i++){
-            str[i]=sc.next();
+        String [] str = new String[n];
+        for(int i=0; i<n;i++){
+            str[i] = sc.next();
         }
-        for(String x : m.solution(str,n)){
+        for(String x : main.solution(n,str)){
             System.out.println(x);
         }
     }
