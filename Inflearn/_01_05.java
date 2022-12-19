b797d9498513d26eb1574934d56896f630c6bdf2
bigwhale1948@gmail.com <bigwhale1948@gmail.com>
2022-12-19 14:31:06 +0900

    [221219 특수단어뒤집기 리팩토링]

--- Inflearn/_01_05.java
+++ Inflearn/_01_05.java
@@ -1,42 +1,41 @@
 import java.util.*;
 
-class Main {
-    public String solution(String str) {
-        String answer = "";
-        char[] s = str.toCharArray();
-        //lt 첫문자 rt 끝문자
-        int lt = 0;
-        int rt = str.length() - 1;
-        while (lt < rt) {
 
-            //문자가 알파벳인지 구분
-            if (!Character.isAlphabetic(s[lt])) lt++;
-            else if (!Character.isAlphabetic(s[rt])) rt--;
-            else {
-                //알파벳이라면 교환하는 tmp
-                char tmp = s[lt];
-                s[lt] = s[rt];
-                s[rt] = tmp;
-                lt++;
-                rt--;
+class Main {
+    public String solution(String input){
+
+        String answer = "";
+
+        int rt = input.length()-1;
+        int lt = 0;
+
+        char[] sc  = input.toCharArray();
+
+        while(lt<rt){
+            // 알파벳이 아닐때
+            if(!Character.isAlphabetic(sc[lt])){
+                lt ++;
+            }else if(!Character.isAlphabetic(sc[rt])){
+                rt --;
+            }else{
+                char tmp = sc[lt];
+                sc[lt] = sc[rt];
+                sc[rt] = tmp;
+                lt ++;
+                rt --;
             }
         }
-        //s를 string화 시켜주는 valueOf
-        answer = String.valueOf(s);
-
-
+        answer=String.valueOf(sc);
         return answer;
     }
 
-
     public static void main(String[] args) {
-        Main T = new Main();
+        Main main = new Main();
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        System.out.println(T.solution(str));
 
+        String input = sc.nextLine();
+
+        System.out.println(main.solution(input));
 
     }
-
-
 }
