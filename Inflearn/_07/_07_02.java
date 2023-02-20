786368702cf0b33fe9c03be78fe6043696f4c647
bigwhale1948@gmail.com <bigwhale1948@gmail.com>
2023-02-20 17:40:47 +0900

    [230220 이진수 출력]

---
+++ Inflearn/_07/_07_02.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public void DFS(int n){
+        if(n==0) return;
+        else{
+            System.out.print(n%2);
+            DFS(n/2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.DFS(11);
+    }
+
+}
